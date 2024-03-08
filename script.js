8d0c6194784f5c230ff804141605f39af7484f1b
FarazHan1466 <160055639+FarazHan1466@users.noreply.github.com>
2024-03-08 22:32:26 +0500

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Even & Odd Numbers
+let number = prompt("Enter The Number: ");
+let numb = Number.parseInt(number);
+
+if (number % 2 == 0) {
+  console.log("Even Number");
+} else {
+  console.log("Odd Number");
+}
